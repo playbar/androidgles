5aacd0eef0bc231ef83bacbdb1e4777d9b38ebdf
playbar <hgl868@126.com>
2018-05-29 16:32:25 +0800

    添加vkview工程，实现native层能用vulkan绘制

---
+++ ASgles3Jni/vkview/src/main/java/com/android/gles3jni/GLES3Hook.java
@@ -0,0 +1,11 @@
+package com.android.gles3jni;
+
+/**
+ * Created by houguoli on 2017/8/15.
+ */
+
+public class GLES3Hook {
+    public static native void initHook();
+    public static native void unInitHook();
+    public static native void hookTest();
+}
